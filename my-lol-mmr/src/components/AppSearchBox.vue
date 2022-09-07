60e5b277b05ae447105e9c6ab706712c8cb0ebe1
Minho Yoo <ymh0951@naver.com>
2022-09-08 00:23:39 +0900

    검색 컴포넌트 개발

--- my-lol-mmr/src/components/AppSearchBox.vue
+++ my-lol-mmr/src/components/AppSearchBox.vue
@@ -1,13 +1,17 @@
 <template>
     <div class="search_box">
-        <select class="country-select">
+        <select class="country-select" v-model="countrySelect">
             <option value="na">NA</option>
             <option value="euw">EUW</option>
             <option value="eune">EUNE</option>
             <option value="kr" selected>KR</option>
         </select>
         <span class="line"></span>
-        <input class="summer-name" placeholder="소환사명">
+        <input class="summer-name" placeholder="소환사명" v-model="summerNameInput" @keyup.enter="summerNameSearch">
+        <div class="icon_box">
+            <i class="fa fas fa-search" v-if="!summerNameInput"></i>
+            <i class="fa fas fa-times" @click="summerNameReset" v-else></i>
+        </div>
     </div>
 </template>
 
@@ -15,7 +19,31 @@
     import Vue from 'vue'
 
     export default Vue.extend({
-        
+        data() {
+            return {
+                countrySelect: 'kr' as string,
+                summerNameInput: '' as string
+            }
+        },
+        methods: {
+            summerNameSearch() {
+                interface DataType {
+                    country: string,
+                    summerName: string,
+                }
+
+                const data: DataType = {
+                    country: this.countrySelect,
+                    summerName: this.summerNameInput
+                }
+
+                console.log('user search', data);
+                this.$emit('userSearch', data);
+            },
+            summerNameReset() {
+                this.summerNameInput = '';
+            }
+        },
     })
 </script>
 
@@ -47,5 +75,14 @@
     margin: 0 10px;
     display: block;
     background-color: #D2D2D2;
+}
+.icon_box {
+    width: 20px;
+}
+.fa {
+    color: #A6A6A6;
+}
+.fa-times {
+    cursor: pointer;
 }
 </style>
